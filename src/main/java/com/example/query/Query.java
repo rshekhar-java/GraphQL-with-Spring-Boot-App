efa3910d1cee335481f6acea750fcfc7f55e8fdf
Ravi Shekhar <rsshekhar27@outlook.com>
2023-01-30 22:35:13 -0600

    GraphQL with Input Data

--- src/main/java/com/example/query/Query.java
+++ src/main/java/com/example/query/Query.java
@@ -1,5 +1,6 @@
 package com.example.query;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Component;
 
@@ -19,4 +20,11 @@ public class Query implements GraphQLQueryResolver {
 	public String secondQuery() {
 		return "Second Query";
 	}
+
+	//Passing Input Data  to GraphQL Query
+	@QueryMapping
+	public String fullName(@Argument String firstName,@Argument String lastName){
+		return firstName+" "+lastName;
+	}
+
 }
