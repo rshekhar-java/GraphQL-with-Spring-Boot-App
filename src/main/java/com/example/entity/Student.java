b8fb5533e7213cc26037e75c19e70b1793f7de27
Ravi Shekhar <rsshekhar27@outlook.com>
2023-01-31 22:41:49 -0600

    Get List of Data with GraphQL Query

--- src/main/java/com/example/entity/Student.java
+++ src/main/java/com/example/entity/Student.java
@@ -1,11 +1,12 @@
 package com.example.entity;
 
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.List;
 
 /**
--- src/main/java/com/example/entity/Subject.java
+++ src/main/java/com/example/entity/Subject.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 /**
  * created by rs 1/31/2023.
